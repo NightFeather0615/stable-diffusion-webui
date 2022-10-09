a7be77f0467adabffa26fe89bb5aefcac9d29dce
NightFeather <leolee50910@gmail.com>
2022-10-09 15:16:52 +0800

    upd

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,25 @@
 @echo off
 
+set useLowVram=null
+
+:askUseLowVram
+
+set /p useLowVram=Use Low VRAM Mode to generate high res. image (Y/N)?: 
+
+if %useLowVram% == y (
+  set COMMANDLINE_ARGS=--lowvram
+  goto :endAskUseLowVram
+) else if %useLowVram% == n (
+  set COMMANDLINE_ARGS=
+  goto :endAskUseLowVram
+) else (
+  goto :askUseLowVram
+)
+
+:endAskUseLowVram
+
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
 
 call webui.bat
